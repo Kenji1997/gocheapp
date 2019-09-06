2b4b5361e0e411f17766599bc5e5f34edcd5109f
Bee <duy.ta@sotatek.com>
2019-09-06 07:05:28 +0700

    add html

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
+"use strict";
+
 $(document).ready(function () {
     var bannerSwiper = new Swiper('.swiper-banner');
+    var longtripSwiper = new Swiper('.swiper-longtrip', {
+        navigation: {
+            nextEl: '.longtrip-button-next',
+            prevEl: '.longtrip-button-prev',
+        },
+    });
 });
